bf274b9d2b933cc0838bedda300a8d1925ec3b63
TNemoo <161608076+TNemoo@users.noreply.github.com>
2024-03-05 17:28:46 +0300

    init

---
+++ src/test/java/com/svl/pastebox/PasteboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svl.pastebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
